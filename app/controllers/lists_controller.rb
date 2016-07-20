06738ccb4458323b3f395f194aad8c94add00c0d
blueplanet <erguolinge@gmail.com>
2016-07-20 20:55:01 +0800

    add controller

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,26 @@
+class ListsController < ApplicationController
+  protect_from_forgery expect: [:update]
+
+  def index
+  end
+
+  def lists
+    @lists = List.all
+    render json: { lists: @lists.to_json(include: :cards) }
+  end
+
+  def update
+    @card = Card.find params[:id]
+    if @card.update card_params
+      render json: { status: 'success' }
+    else
+      render json: { status: 'failure' }
+    end
+  end
+
+  private
+
+    def card_params
+      parmas.require(:card).permit(:list_id)
+    end
+end
