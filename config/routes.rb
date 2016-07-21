be4fdbf47120a0d6c23657c9512a51c5831df8c7
blueplanet <erguolinge@gmail.com>
2016-07-21 19:44:43 +0800

    Add route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'lists/index'
+  root 'lists/index'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :lists, only: %i(index update) do
+    get :lists, on: :collection
+  end
 end
